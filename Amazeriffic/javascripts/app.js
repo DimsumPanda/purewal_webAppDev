8f2a2a5f2dd6fd9481304b0320c753caab3138b5
Barbara <b.poon@umiami.edu>
2016-07-24 16:40:09 -0500

    added a toDos array that will appear for second tab

--- Amazeriffic/javascripts/app.js
+++ Amazeriffic/javascripts/app.js
@@ -1,10 +1,20 @@
 var main = function () {
 	"use strict";
 
+	var toDos = [
+		"Finish writing this book",
+		"Take Gracie to the park",
+		"Answeer emails",
+		"Prep for Monday's class",
+		"Make up some new ToDos",
+		"Get Groceries"
+	];
+
 	$(".tabs a span").toArray().forEach(function (element){
 		//create a click handler for this element
 		$(element).on("click", function(){
-			var $element = $(element);
+			var $element = $(element), 
+							$content;
 
 			$(".tabs a span").removeClass("active");
 			$(element).addClass("active");
@@ -13,7 +23,11 @@ var main = function () {
 			if ($element.parent().is(":nth-child(1)")){
 				console.log("First Tab clicked!");
 			} else if ($element.parent().is(":nth-child(2)")){
-				console.log("Second Tab clicked!");
+				$content = $("<ul>");
+				toDos.forEach(function(todo) {
+					$content.append($"<li>").text(todo));
+				});
+				$("main .content").append($content);
 			} else if ($element.parent().is(":nth-child(3)")){
 				console.log("Third Tab clicked!");
 			}

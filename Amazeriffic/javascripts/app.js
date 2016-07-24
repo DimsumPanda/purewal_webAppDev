38fd739a80daa8a5bc12c60196379f26b76ebb72
Barbara <b.poon@umiami.edu>
2016-07-24 16:34:46 -0500

    added some styling the li elements

--- Amazeriffic/javascripts/app.js
+++ Amazeriffic/javascripts/app.js
@@ -4,9 +4,20 @@ var main = function () {
 	$(".tabs a span").toArray().forEach(function (element){
 		//create a click handler for this element
 		$(element).on("click", function(){
+			var $element = $(element);
+
 			$(".tabs a span").removeClass("active");
 			$(element).addClass("active");
 			$("main .content").empty();
+			
+			if ($element.parent().is(":nth-child(1)")){
+				console.log("First Tab clicked!");
+			} else if ($element.parent().is(":nth-child(2)")){
+				console.log("Second Tab clicked!");
+			} else if ($element.parent().is(":nth-child(3)")){
+				console.log("Third Tab clicked!");
+			}
+
 			return false;
 		});
 	});
